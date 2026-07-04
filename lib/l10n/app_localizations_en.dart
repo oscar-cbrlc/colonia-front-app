a76b23359fbbf7e9d78cfa76f7bde8a843cfb8dc
oscar-cbrlc <oscar.cbrlc@gmail.com>
2026-07-03 21:49:22 -0600

    feat: es & en language text function

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,35 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get loginOrSignUp => 'Login or sign up';
+
+  @override
+  String get continueWithEmail => 'Continue with Email';
+
+  @override
+  String get selectYourAuthPref =>
+      'Select your preferred method to continue setting up your account';
+
+  @override
+  String get ifCreatingNewAccount => 'If you are creating a new account, ';
+
+  @override
+  String get termsAndConditions => 'Terms & Conditions';
+
+  @override
+  String get and => 'and';
+
+  @override
+  String get privacyPolicy => 'Privace Policy';
+
+  @override
+  String get willApply => 'will apply';
+}
---
+++ lib/l10n/app_localizations_es.dart
@@ -0,0 +1,35 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get loginOrSignUp => 'Iniciar sesión o registrarse';
+
+  @override
+  String get continueWithEmail => 'Continuar con Email';
+
+  @override
+  String get selectYourAuthPref =>
+      'Selecciona tu método de preferencia para iniciar tu cuenta';
+
+  @override
+  String get ifCreatingNewAccount => 'Si estás creando una nueva cuenta, ';
+
+  @override
+  String get termsAndConditions => 'Términos y Condiciones';
+
+  @override
+  String get and => 'y';
+
+  @override
+  String get privacyPolicy => 'Política de Privacidad';
+
+  @override
+  String get willApply => 'se aplicarán';
+}
