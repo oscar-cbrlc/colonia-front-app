879b0799c688e3d58c6af4203536d6cf2d82d095
oscar-cbrlc <oscar.cbrlc@gmail.com>
2026-07-09 17:09:50 -0600

    feat(auth): add providers to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'ui/auth/widgets/welcome_screen.dart';
-import 'package:colonia_front_app/ui/auth/view_models/welcome_viewmodel.dart';
-import 'package:colonia_front_app/ui/core/themes/app_theme.dart';
-import 'package:colonia_front_app/l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'package:colonia_front_app/l10n/app_localizations.dart';
+import 'package:colonia_front_app/ui/core/themes/app_theme.dart';
 
-void main() => runApp(const ColoniaApp());
+import 'package:colonia_front_app/data/services/api/api_client.dart';
+import 'package:colonia_front_app/data/services/auth_service.dart';
+import 'package:colonia_front_app/data/repositories/auth_repository.dart';
+
+import 'package:colonia_front_app/ui/auth/widgets/welcome_screen.dart';
+import 'package:colonia_front_app/ui/auth/view_models/welcome_viewmodel.dart';
+
+
+void main(){
+  runApp(
+    MultiProvider(
+        providers: [
+          Provider<ApiClient>(
+            create: (_) => ApiClient(),
+          ),
+          ProxyProvider<ApiClient, AuthService>(
+            update: (_, apiClient, __) => AuthService(apiClient),
+          ),
+          ProxyProvider<AuthService, AuthRepository>(
+            update: (context, authService, __) {
+              final authRepository = AuthRepository(authService);
+              final apiClient = Provider.of<ApiClient>(context, listen: false);
+              apiClient.setAuthRepository(authRepository);
+
+              return authRepository;
+            },
+          ),
+        ],
+      child: const ColoniaApp(),
+    )
+  );
+}
 
 class ColoniaApp extends StatelessWidget {
   const ColoniaApp({super.key});
