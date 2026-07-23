37ba6cbd300e65756428b091357e32dc13bfad8f
Oscar Cabrales <oscar.cbrlc@gmail.com>
2026-07-22 18:24:04 -0600

    feat(map): add game config

---
+++ lib/config/game_config.dart
@@ -0,0 +1,6 @@
+abstract class GameConfig {
+  static const int h3Resolution = 10;
+
+  static const double minMetersBetweenVertices = 1.0;
+  static const Duration gpsUpdateInterval = Duration(seconds: 2);
+}
