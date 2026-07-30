a12d254beb7a51bd6b5d92a80d5486c2a6547c03
Oscar Cabrales <oscar.cbrlc@gmail.com>
2026-07-29 18:17:55 -0600

    feat(activity): add territory model

---
+++ lib/domain/models/territory.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'territory.freezed.dart';
+part 'territory.g.dart';
+
+@freezed
+abstract class Territory with _$Territory {
+  const Territory._();
+  const factory Territory({
+    @JsonKey(name: 'territory_id') required String id,
+    @JsonKey(name: 'team_id') required int teamId,
+    @JsonKey(name: 'health_points') required double healthPoints
+  }) = _Territory;
+
+  factory Territory.fromJson(Map<String, dynamic> json) => _$TerritoryFromJson(json);
+}
