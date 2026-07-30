e12577797feaa166bb2e7fa5a01f42fdc15361e3
Oscar Cabrales <oscar.cbrlc@gmail.com>
2026-07-29 18:19:48 -0600

    feat(activity): add onTrackNode model

---
+++ lib/domain/models/session/on_track_node.dart
@@ -0,0 +1,13 @@
+class OnTrackNode  {
+  final double lat;
+  final double lon;
+  final double pace;
+  final DateTime timestamp;
+
+  OnTrackNode({
+    required this.lat,
+    required this.lon,
+    required this.pace,
+    required this.timestamp,
+  });
+}
