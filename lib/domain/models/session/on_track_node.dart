00baf48de152a872029917cb52cf9a9bedbf30e9
Oscar Cabrales <oscar.cbrlc@gmail.com>
2026-08-11 20:13:20 -0600

    feat(activity): add points to track node

--- lib/domain/models/session/on_track_node.dart
+++ lib/domain/models/session/on_track_node.dart
@@ -2,12 +2,14 @@ class OnTrackNode  {
   final double lat;
   final double lon;
   final double pace;
+  final double points;
   final DateTime timestamp;
 
   OnTrackNode({
     required this.lat,
     required this.lon,
     required this.pace,
+    required this.points,
     required this.timestamp,
   });
 }
