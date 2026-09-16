ef840219943286f258a1cf98198d03a4ae022294
Oscar Cabrales <oscar.cbrlc@gmail.com>
2026-09-16 00:41:40 -0600

    feat(storage): add pending activity storage model

---
+++ lib/domain/models/session/pending_activity_impact.dart
@@ -0,0 +1,31 @@
+class PendingActivityImpact {
+  final double totalDistance;
+  final int totalTime;
+  final String timestamp;
+  final List<Map<String, dynamic>> territories;
+  final int? boostId;
+
+  PendingActivityImpact({
+    required this.totalDistance,
+    required this.totalTime,
+    required this.timestamp,
+    required this.territories,
+    this.boostId,
+  });
+
+  Map<String, dynamic> toJson() => {
+    'total_distance': totalDistance,
+    'total_time': totalTime,
+    'timestamp': timestamp,
+    'territories': territories,
+    'boost_id': boostId,
+  };
+
+  factory PendingActivityImpact.fromJson(Map<String, dynamic> json) => PendingActivityImpact(
+    totalDistance: (json['total_distance'] as num).toDouble(),
+    totalTime: (json['total_time'] as num).toInt(),
+    timestamp: json['timestamp'] as String,
+    territories: List<Map<String, dynamic>>.from(json['territories'] as List),
+    boostId: json['boost_id'] as int?,
+  );
+}
